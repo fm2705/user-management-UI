2188a4140c04f697b10168ad0b7475b13b355dd4
Ludmilla <ludmillajani@yahoo.com>
2022-10-09 13:45:43 -0400

    Create project UI structure

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>User management App</title>      
+      </Head>
+
+      <main>
+      </main>
+
+    </div>
+  )
+}
